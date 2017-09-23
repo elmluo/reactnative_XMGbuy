5b5eda88bbd1d42e31368fb0123cfca6cb9b6ed8
luoc <luoc@kongtrolink.com>
2017-09-23 18:18:51 +0800

    [add] add tabbar new stayles, include tabbarStyle  renderBadge, newBadageStyle

--- app/Main.js
+++ app/Main.js
@@ -1,11 +1,10 @@
 
 import React, { Component } from 'react';
-import { StyleSheet, Image, Platform } from 'react-native';
-
-// 从大模块儿中引入需要的功能模块儿
+import { StyleSheet, Image, View, Text, Platform } from 'react-native';
 // 一定要要使用解构
 import { Navigator } from 'react-native-deprecated-custom-components'
 import TabNavigator from 'react-native-tab-navigator';
+// 从大模块儿中引入需要的功能模块儿
 import Home from './Home/Home';
 import Shop from './Shop/Shop';
 import Mine from './Mine/Mine';
@@ -20,7 +19,11 @@ export default class Main extends Component {
     }
     render() {
         return (
-            <TabNavigator>
+            <TabNavigator
+                sceneStyle={styles.sceneStyle}
+                tabBarStyle={styles.tabBarStyle}
+                tabBarShadowStyle={styles.tabBarShadowStyle}
+                hidesTabTouch={false}>
                 <TabNavigator.Item
                     title="首页"
                     titleStyle={styles.titleStyle}
@@ -28,9 +31,16 @@ export default class Main extends Component {
                     renderIcon={() => <Image source={require('./assets/img/icon_tabbar_homepage.png')} style={styles.iconStyle}/>}
                     renderSelectedIcon={() => <Image source={require('./assets/img/icon_tabbar_homepage_selected.png')} style={styles.iconStyle}/>}
                     badgeText="1"
+                    renderBadge={()=>{
+                        return (
+                            <View style={styles.badge}>
+                                <Text style={styles.badgeText}>1</Text>
+                            </View>
+                        )
+                    }}
                     onPress={() => this.setState({ selectedTab: 'home' })}
-                    selected={this.state.selectedTab === 'home'}>
-
+                    selected={this.state.selectedTab === 'home'}
+                    allowFontScaling={true}>
                     <Navigator
                         initialRoute={{name:'首页',component:Home}}
                         configureScene={()=>{// 过渡动画
@@ -50,6 +60,12 @@ export default class Main extends Component {
                     renderIcon={() => <Image source={require('./assets/img/icon_tabbar_merchant_normal.png')} style={styles.iconStyle}/>}
                     renderSelectedIcon={() => <Image source={require('./assets/img/icon_tabbar_merchant_selected.png')} style={styles.iconStyle}/>}
                     badgeText="1"
+                    renderBadge={()=>{
+                        return (
+                            <View style={styles.badge}>
+                            </View>
+                        )
+                    }}
                     onPress={() => this.setState({ selectedTab: 'shop' })}>
                     <Shop/>
                 </TabNavigator.Item>
@@ -74,7 +90,6 @@ export default class Main extends Component {
                     onPress={() => this.setState({ selectedTab: 'more' })}>
                     <More/>
                 </TabNavigator.Item>
-
             </TabNavigator>
         );
     }
@@ -87,14 +102,43 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: '#F5FCFF',
     },
+    sceneStyle: {
+    },
+    tabBarStyle: {
+        backgroundColor: "pink",
+        // height: 100,
+        // marginTop: 10,
+        // paddingTop: 10,
+        // alignItems: 'center'
+    },
+    tabBarShadowStyle: {
+    },
     titleStyle: {
     },
     selectedTitleStyle: {
         color: 'orange',
     },
+    badge: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        top: -2,
+        right: -5,
+        width: 14,
+        height: 14,
+        borderWidth: 2,
+        borderColor: '#FFFFFF',
+        borderRadius: 7,
+        backgroundColor: 'red',
+    },
+    badgeText: {
+        fontSize: 10,
+        color: '#DDDDDD'
+    },
     iconStyle: {
-        width: Platform.OS ==='ios' ? 30 : 30,
-        height: Platform.OS === 'ios' ? 30 : 30,
+        width: Platform.OS ==='ios' ? 30 : 25,
+        height: Platform.OS === 'ios' ? 30 : 25,
     }
 });
 
