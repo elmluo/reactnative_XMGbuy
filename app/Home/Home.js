4e6b18b02cdeea8168b415e0fdba973008df0007
luoc <luoc@kongtrolink.com>
2017-09-25 14:09:29 +0800

    [add] finish navbar function

--- app/Home/Home.js
+++ app/Home/Home.js
@@ -9,8 +9,11 @@ import {
     TouchableOpacity,
     BackHandler,
     Platform,
+    Dimensions,
 } from 'react-native';
-import HomeDetail from './Home_detail'
+import HomeDetail from './Home_detail';
+import HomeArea from './Home_Area';
+let {width, height} = Dimensions.get('window');
 export default class Home extends Component {
     constructor(props){
         super(props);
@@ -24,6 +27,7 @@ export default class Home extends Component {
             <View style={styles.container}>
                 {/*首页导航条*/}
                 {this._renderNavBar()}
+
                 <Text>
                     我是Home页面
                 </Text>
@@ -56,16 +60,41 @@ export default class Home extends Component {
      */
     _renderNavBar() {
         return(
-            <View>
-                <View>
-                    <Text>广州</Text>
-                </View>
+            <View style={styles.nav}>
+                <TouchableOpacity
+                    onPress={()=>{
+                        this.props.navigator.push({
+                            component: HomeArea,
+                            title: 'Area'
+                        })
+                    }}>
+                    <View style={styles.navLeft}>
+                        <Text>广州</Text>
+                    </View>
+                </TouchableOpacity>
                 <View>
                     <TextInput
                         placeholder="输入商家爱，品类，商圈"
+                        underlineColorAndroid='transparent'
                         style={styles.navTextInput}/>
                 </View>
-                <View></View>
+                <View style={styles.navRight}>
+                    <TouchableOpacity
+                        onPress={()=>{
+                            alert('点击了')
+                        }}>
+                        <Image source={require('../assets/img/icon_homepage_message.png')}
+                               style={styles.navRightImg}/>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={()=>{
+                            alert('点击了')
+                        }}>
+                        <Image source={require('../assets/img/icon_homepage_scan.png')}
+                               style={styles.navRightImg}/>
+                    </TouchableOpacity>
+
+                </View>
             </View>
         )
     }
@@ -77,10 +106,29 @@ const styles = StyleSheet.create({
         backgroundColor: '#F5FCFF',
     },
     nav: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        height: Platform.OS==="ios" ? 66 : 44,
+        backgroundColor: 'rgba(255, 96, 0, 1.0)',
+    },
+    navLeft: {
 
     },
-    navInput: {
-
+    navTextInput: {
+        height: 30,
+        width: width * 0.7,
+        backgroundColor: '#FFFFFF',
+        borderRadius: 22,
+        paddingLeft: 15,
+        padding: 0,
+    },
+    navRight: {
+      flexDirection: 'row'
+    },
+    navRightImg: {
+        width: 30,
+        height: 30
     }
 
 });
---
+++ app/Home/Home_Area.js
@@ -0,0 +1,77 @@
+
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    TouchableOpacity,
+    BackHandler,
+    Platform,
+} from 'react-native';
+
+export default class HomeDetail extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    componentWillMount() {
+        if (Platform.OS === 'android') {
+            BackHandler.addEventListener('hardwareBackPress', this._onBackAndroid);
+        }
+    }
+
+    componentWillUnmount() {
+        if (Platform.OS === 'android') {
+            BackHandler.removeEventListener('hardwareBackPress', this._onBackAndroid)
+        }
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>
+                    我是地区选择页页
+                </Text>
+                <TouchableOpacity
+                    onPress={()=> {
+                        // 跳转回父组件
+                        this.props.navigator.pop()
+                    }}>
+                    <Text>
+                        点我跳转回父页面
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+
+    /**
+     * 点击android物理返回建操作
+     * @returns {boolean}
+     * @private
+     */
+    _onBackAndroid = ()=> {
+        const navigator = this.props.navigator;
+        const routers = navigator.getCurrentRoutes();
+        console.log("当前路由长度");
+        if (routers.length > 1 ) {
+            navigator.pop();
+            return true;
+        }
+        return false;
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+    },
+});
+
